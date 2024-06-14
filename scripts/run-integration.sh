824ebc829705554b031ee3434441ddf70316513f
omkardongre <omkardongre5@gmail.com>
2024-06-14 09:46:27 +0530

    message

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,7 +1,16 @@
+#!/usr/bin/sh
+
+# Print the shell being used to execute this script
+echo "Running script with shell: $(ps -p $$ -o comm=)"
+
+# Print the current shell to verify
+echo "Current shell is: $SHELL"
+
 docker compose up -d
 
 echo '🟡 - Waiting for database to be ready...'
-./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
+bash ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
+
 
 npx prisma migrate dev --name init
 npm run test
